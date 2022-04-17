18fc4bcb100af8427b459e4b952155733de9afde
Hanz-Nek <Nekucfyekesh.f@gmail.com>
2022-04-17 18:31:58 +0300

    Kurs_12_04_2022

---
+++ Kurs_12_04_2022/proj12042022/src/main/java/proj12042022/HibernateUtil.java
@@ -0,0 +1,18 @@
+package proj12042022;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class HibernateUtil {
+    public static final EntityManagerFactory emFactory;
+    static {
+        emFactory = Persistence.createEntityManagerFactory("mainDB");
+    }
+    public static EntityManager getEntityManager (){
+        return emFactory.createEntityManager();
+    }
+    public static void close(){
+        emFactory.close();
+    }
+}
